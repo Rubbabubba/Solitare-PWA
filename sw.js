9903dc85e8c499d959d0b5816fa86241243c45a7
Rubbabubba <47430510+Rubbabubba@users.noreply.github.com>
2026-01-18 22:26:49 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Versioned cache. Bump CACHE_VERSION when you change files. */
-const CACHE_VERSION = 'solitaire-v2';
+const CACHE_VERSION = 'solitaire-v3';
 const CORE = [
   './',
   './index.html?v=1',
